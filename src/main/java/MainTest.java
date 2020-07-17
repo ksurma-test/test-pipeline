52b8a5fecb05c1bbe985b7b03d5937f973c1807c
Kamil Surma <kamil.surma@wp.pl>
2020-07-17 23:09:34 +0200

    Pierwsza klasa

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,17 @@
+public class MainTest {
+
+
+    private static String imie = "Kamil";
+    private static String nazwisko = "Surma";
+
+
+    public MainTest() {
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(imie + " " + nazwisko);
+
+    }
+
+}
